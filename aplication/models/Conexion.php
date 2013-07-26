f644c5dcc927addc3bc54c690213288955493f55
oscar <oscar.grimaldo@itcelaya.edu.mx>
2013-07-26 10:20:03 -0500

    archivo conexion

---
+++ aplication/models/Conexion.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conexion
+ *
+ * @author ozkar
+ */
+class Conexion {
+    
+    
+    function Conexion(){
+        $this->db = ADONewConnection('mysql');
+        $this->db->debug = false;
+        $this->db->Connect('localhost','root','root','EventosITC');
+    }
+    
+}
+
+?>
